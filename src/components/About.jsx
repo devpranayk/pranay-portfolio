f1175e4161df987b0f994e3021b0caf23857d016
Pranay Kandikonda <pranayk74174174@gmail.com>
2026-02-17 20:56:45 +0530

    code readability increased

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -61,15 +61,24 @@ export default function About() {
         </motion.div>
 
                                             {/* RIGHT : TEXT */}
+
         <motion.div
+
           initial={{ opacity: 0, x: 40 }}
+
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
+
           viewport={{ once: true }}
+
+
         >
+
           <h2 className="text-4xl md:text-5xl font-hero leading-tight">
             About Me
           </h2>
+
+
 
           <p className="mt-8 text-lg leading-relaxed text-gray-700 dark:text-gray-400 max-w-xl">
             I’m a Full Stack Developer who builds scalable, end-to-end web
@@ -77,15 +86,21 @@ export default function About() {
             and cloud-ready deployments.
           </p>
 
+
           <p className="mt-6 text-lg leading-relaxed text-gray-600 dark:text-gray-400 max-w-xl">
             I work across frontend, backend, databases, and cloud infrastructure,
             combining engineering discipline with strong UI/UX sensibility.
             I’m also a Google Cloud Certified Associate Cloud Engineer,
             with a solid understanding of modern cloud systems.
           </p>
+
+
         </motion.div>
 
+
       </div>
+      
     </section>
+
   )
 }
